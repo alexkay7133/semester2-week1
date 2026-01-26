9bfe5073bce614bd67033295656085cef304ff71
alexkay7133 <rsxr0509@leeds.ac.uk>
2026-01-26 12:56:08 +0000

    Finished session 1

--- session_1/1_country/task1.sql
+++ session_1/1_country/task1.sql
@@ -9,7 +9,29 @@
 -- 2. Load this script: .read task1.sql
 -- 3. Exit SQLite: .exit
 
+-- Continent      Population  AreaSqMi  PopDensityPerSqMi  CoastlineRatio  NetMigration  
+--InfantMortalityPer1000  GDPPerCapita  LiteracyPercent  PhonesPer1000  ArablePercent  
+--CropsPercent  OtherPercent  Climate  Birthrate  Deathrate  Agriculture  Industry  
+--Service
 
 -- write your sql code here
 
-SELECT * from countries;
+-- SELECT * FROM countries;
+
+-- SELECT Country, Population FROM countries;
+
+-- SELECT Country FROM countries WHERE Continent='Europe';
+
+-- SELECT Country FROM countries WHERE Population > 100000000;
+
+-- SELECT Country FROM countries WHERE Continent='Asia' AND GDPPerCapita > 20000;
+
+-- SELECT Country FROM countries WHERE LiteracyPercent < 70;
+
+-- SELECT Country FROM countries WHERE CoastlineRatio = 0;
+
+-- SELECT Country FROM countries WHERE Country LIKE 'A%';
+
+-- SELECT Country FROM countries WHERE Country LIKE '%United%';
+
+-- SELECT Country FROM countries WHERE Birthrate > Deathrate AND Population > 50000000;
--- session_1/1_country/task2.sql
+++ session_1/1_country/task2.sql
@@ -9,6 +9,20 @@
 -- 2. Load this script: .read task2.sql
 -- 3. Exit SQLite: .exit
 
+-- Continent      Population  AreaSqMi  PopDensityPerSqMi  CoastlineRatio  NetMigration  
+--InfantMortalityPer1000  GDPPerCapita  LiteracyPercent  PhonesPer1000  ArablePercent  
+--CropsPercent  OtherPercent  Climate  Birthrate  Deathrate  Agriculture  Industry  
+--Service
 
 -- write your sql code here
 
+-- SELECT Country FROM countries ORDER BY Population DESC LIMIT 10;
+-- SELECT Country FROM countries ORDER BY AreaSqMi ASC LIMIT 5;
+-- SELECT Country FROM countries WHERE Continent='Europe' ORDER BY AreaSqMi DESC LIMIT 1;
+-- SELECT Country FROM countries WHERE Continent='Africa' ORDER BY AreaSqMi ASC LIMIT 3;
+-- SELECT Country FROM countries ORDER BY LiteracyPercent ASC LIMIT 5;
+-- SELECT Country FROM countries WHERE Country LIKE 'C%' ORDER BY GDPPerCapita DESC LIMIT 3;
+-- SELECT Country FROM countries WHERE InfantMortalityPer1000 > 50 ORDER BY LiteracyPercent DESC LIMIT 1;
+-- SELECT Country FROM countries WHERE GDPPerCapita > 10000 ORDER BY PhonesPer1000 ASC LIMIT 5;
+-- SELECT Country FROM countries WHERE Continent='South America' AND CoastlineRatio = 0 ORDER BY GDPPerCapita DESC LIMIT 1;
+-- SELECT Country FROM countries ORDER BY Country DESC LIMIT 10;
