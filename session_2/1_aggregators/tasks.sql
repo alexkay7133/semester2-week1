94d7370537216309f77a6428a22b9347d1bb14eb
alexkay7133 <rsxr0509@leeds.ac.uk>
2026-01-30 10:49:52 +0000

    finished session 2

--- session_2/1_aggregators/tasks.sql
+++ session_2/1_aggregators/tasks.sql
@@ -7,3 +7,19 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+-- Continent      Population  AreaSqMi  PopDensityPerSqMi  CoastlineRatio  NetMigration  
+--InfantMortalityPer1000  GDPPerCapita  LiteracyPercent  PhonesPer1000  ArablePercent  
+--CropsPercent  OtherPercent  Climate  Birthrate  Deathrate  Agriculture  Industry  
+--Service
+
+--SELECT Continent, AVG(Population) FROM countries GROUP BY Continent;
+--SELECT Continent, SUM(Population) FROM countries GROUP BY Continent;
+--SELECT Continent, MAX(Population) FROM countries GROUP BY Continent;
+--SELECT Continent, MIN(AreaSqMi) FROM countries GROUP BY Continent;
+--SELECT Continent, COUNT(Country) FROM countries GROUP BY Continent;
+--SELECT Continent, AVG(GDPPerCapita) FROM countries GROUP BY Continent ORDER BY AVG(GDPPerCapita) DESC;
+--SELECT Country, Population FROM countries ORDER BY Population DESC LIMIT 5;
+--SELECT Country, InfantMortalityPer1000 FROM countries ORDER BY InfantMortalityPer1000 ASC LIMIT 5;
+--SELECT Continent, AVG(LiteracyPercent) FROM countries GROUP BY Continent ORDER BY AVG(LiteracyPercent) ASC;
+--SELECT Continent, SUM(Population) FROM countries GROUP BY Continent ORDER BY SUM(Population) DESC LIMIT 3;
+--SELECT Continent, AVG(Population) FROM countries WHERE Population > 10000000 GROUP BY Continent;
--- session_2/2_joins/tasks.sql
+++ session_2/2_joins/tasks.sql
@@ -7,3 +7,13 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+-- follows: following_user_id followed_user_id
+-- users: id username role created_on
+-- posts: id content user_id deleted created_on
+
+--SELECT content FROM posts WHERE deleted;
+--SELECT * FROM users WHERE username LIKE 'a%';
+--SELECT content, users.username FROM posts JOIN users ON posts.user_id=users.id WHERE content LIKE '%small%';
+--SELECT content, users.username FROM posts JOIN users ON posts.user_id=users.id WHERE deleted=0 ORDER BY posts.created_on ASC;
+--SELECT username, COUNT(posts.content) FROM users JOIN posts ON users.id=posts.user_id GROUP BY username;
+--SELECT username, COUNT(posts.content) FROM users JOIN posts ON users.id=posts.user_id GROUP BY username HAVING COUNT(posts.content) > 3;
