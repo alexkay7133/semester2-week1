e2d35c2b870a0c2b8baa732436aa1db35049bcef
alexkay7133 <rsxr0509@leeds.ac.uk>
2026-02-01 11:19:31 +0000

    Worksheet attempt 1

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,8 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+SELECT Department.DepartmentName, COUNT(Enrolment.StudentId) AS TotalEnrolments
+FROM Course 
+JOIN Department ON Course.DepartmentId = Department.DepartmentId
+JOIN Enrolment ON Course.CourseId = Enrolment.CourseId
+GROUP BY Course.DepartmentId;
--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -1,3 +1,11 @@
--- For each student, calculate the total number of credits from courses they passed. Assume a passing grade is 40 or higher.
+-- For each student, calculate the total number of credits from 
+--courses they passed. Assume a passing grade is 40 or higher.
+
 -- Expected Columns:
 -- StudentId, FirstName, LastName, TotalCreditsPassed
+SELECT Student.StudentId, FirstName, LastName, SUM(Course.Credits) AS TotalCreditsPassed
+FROM Student
+JOIN Enrolment ON Student.StudentId = Enrolment.StudentId
+JOIN Course ON Enrolment.CourseId = Course.CourseId
+GROUP BY Student.StudentId
+HAVING Enrolment.Grade >= 40
